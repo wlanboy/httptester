33ee8de07fe7bc97193e9b45f6a5b775e3a32501
wlanboy <wlanboy@github.com>
2025-03-29 13:36:27 +0100

    init

---
+++ server.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template_string
+import requests
+
+app = Flask(__name__)
+
+# HTML-Seite
+HTML = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>GET Request Tester</title>
+</head>
+<body>
+    <h1>GET Request Tester</h1>
+    <form method="post">
+        <label for="url">URL:</label>
+        <input type="text" id="url" name="url" required>
+        <button type="submit">Senden</button>
+    </form>
+    {% if response %}
+        <h2>Response</h2>
+        <textarea rows="10" cols="80">{{ response }}</textarea>
+        <h2>Headers</h2>
+        <textarea rows="10" cols="80">{{ headers }}</textarea>
+    {% endif %}
+</body>
+</html>
+"""
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    response_text = ""
+    response_headers = ""
+    if request.method == "POST":
+        url = request.form.get("url")
+        try:
+            res = requests.get(url)
+            response_text = res.text
+            response_headers = res.headers
+        except Exception as e:
+            response_text = f"Fehler: {e}"
+            response_headers = ""
+
+    return render_template_string(HTML, response=response_text, headers=response_headers)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ service.sh
@@ -0,0 +1,41 @@
+kubectl create namespace demo
+kubectl label namespace demo istio-injection=enabled
+kubectl create deployment tester --image=wlanboy/http-tester:latest -n demo
+kubectl expose deployment tester --type=ClusterIP --port=5000 -n demo
+kubectl set image deployment/tester 'wlanboy/http-tester:latest' -n demo
+istioctl proxy-status
+
+kubectl get all -n demo
+kubectl describe svc tester -n demo
+kubectl get svc istio-ingress -n istio-ingress
+
+curl tester.demo.svc:5000
+curl tester.local:5000
+curl -X POST -d "url=http://helloworld.sample.svc:5000/hello" tester.demo.svc:5000
+curl -X POST -d "url=http://tester.local/" tester.demo.svc:5000
+
+
+istioctl proxy-status
+
+echo "apiVersion: networking.istio.io/v1
+kind: ServiceEntry
+metadata:
+  name: tester-svc
+spec:
+  hosts:
+  - tester.local
+  location: MESH_INTERNAL
+  ports:
+  - number: 5000
+    name: http
+    protocol: HTTP
+    targetPort: 5000
+  resolution: DNS
+  workloadSelector:
+    labels:
+      app: debian-client" > virtualvmservice.yaml
+
+kubectl label namespace client istio-injection=enabled
+kubectl apply -f virtualvmservice.yaml -n client
+kubectl describe serviceentry tester-svc -n client
+kubectl delete serviceentry tester-svc -n client
