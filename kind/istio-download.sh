eb3c304efca0d6ae29c714cc49dc74e90aafa116
wlanboy <wlanboy@github.com>
2025-06-14 06:28:16 +0000

    added check if folder is present

--- kind/istio-download.sh
+++ kind/istio-download.sh
@@ -11,6 +11,14 @@ download_istio() {
     istio_release_url="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-linux-amd64.tar.gz"
     echo "Lade von: $istio_release_url"
 
+    if [ -n "$ISTIO_DIR" ] && [ -d "$ISTIO_DIR" ]; then
+        echo "Verzeichnis '$ISTIO_DIR' existiert bereits. Überspringe den Download."
+        # Ensure the PATH is set even if no download occurred
+        export PATH=$PWD/$ISTIO_DIR/bin:$PATH
+        echo "Istio-Binärdateien zum PATH hinzugefügt (Verzeichnis existierte bereits)."
+        return 0 # Exit the function successfully
+    fi
+    
     mkdir -p "$ISTIO_DIR"
     curl -L "$istio_release_url" | tar xz --strip-components=1 -C "$ISTIO_DIR"
     if [ $? -ne 0 ]; then
