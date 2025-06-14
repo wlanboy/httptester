5f8a27926a2c223960c9c101d604dffc7ea975c2
wlanboy <wlanboy@github.com>
2025-06-14 08:24:29 +0000

    added ips for gateways and controle plane

--- kind/mesh.sh
+++ kind/mesh.sh
@@ -1,58 +1,40 @@
 #!/bin/bash
 
-# --- Variablen ---
+ISTIO_DIR=./istio
+ISTIO_SYSTEM_NAMESPACE="istio-system"
+export PATH=$PWD/$ISTIO_DIR/bin:$PATH
+
 KIND_CLUSTER_EAST="east"
 KIND_CLUSTER_WEST="west"
-ISTIO_SYSTEM_NAMESPACE="istio-system"
+
 MESH_ID=servicemesh
 MESH_NETWORK=servicenetwork
 
-get_load_balancer_ip() {
-    local cluster_name=$1
-    local service_name=$2
-    local namespace=$3
-    local ip=""
-    echo "Warte auf External IP für $service_name in Cluster $cluster_name..."
-    for i in $(seq 1 60); do # Warte bis zu 5 Minuten
-        ip=$(kubectl --context "kind-$cluster_name" get svc "$service_name" -n "$namespace" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-        if [ -n "$ip" ]; then
-            echo "External IP für $service_name auf Cluster $cluster_name: $ip"
-            echo "$ip"
-            return 0
-        fi
-        echo "Warte auf External IP für $service_name ($i/60)..."
-        sleep 5
-    done
-    echo "Fehler: Konnte keine External IP für $service_name auf Cluster $cluster_name ermitteln."
-    return 1
-}
-
-echo "--- Starte Primary-Primary Service Mesh Konfiguration ---"
-
-# External IPs der East-West Gateways ermitteln
+# External IPs der East-West Gateways und controle-plane ermitteln
 echo "Ermittle External IPs der East-West Gateways..."
-EAST_WEST_GATEWAY_IP_EAST=$(get_load_balancer_ip "$KIND_CLUSTER_EAST" "eastwestgateway" "$ISTIO_SYSTEM_NAMESPACE")
-if [ $? -ne 0 ]; then
-    echo "Beende Skript aufgrund fehlender East-West Gateway IP auf Cluster $KIND_CLUSTER_EAST."
-    exit 1
-fi
+EAST_WEST_GATEWAY_IP_EAST=$(kubectl --context "kind-$KIND_CLUSTER_EAST" get svc "eastwestgateway" -n "$ISTIO_SYSTEM_NAMESPACE" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+EAST_WEST_GATEWAY_IP_WEST=$(kubectl --context "kind-$KIND_CLUSTER_WEST" get svc "eastwestgateway" -n "$ISTIO_SYSTEM_NAMESPACE" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-EAST_WEST_GATEWAY_IP_WEST=$(get_load_balancer_ip "$KIND_CLUSTER_WEST" "eastwestgateway" "$ISTIO_SYSTEM_NAMESPACE")
-if [ $? -ne 0 ]; then
-    echo "Beende Skript aufgrund fehlender East-West Gateway IP auf Cluster $KIND_CLUSTER_WEST."
-    exit 1
-fi
+control_plane_node_name=$(kubectl get nodes --context "kind-$KIND_CLUSTER_WEST" -l node-role.kubernetes.io/control-plane -o custom-columns=NAME:.metadata.name --no-headers)
+echo "$control_plane_node_name"
+IP_EAST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${control_plane_node_name}" 2>/dev/null)
+
+control_plane_node_name=$(kubectl get nodes --context "kind-$KIND_CLUSTER_WEST" -l node-role.kubernetes.io/control-plane -o custom-columns=NAME:.metadata.name --no-headers)
+echo "$control_plane_node_name"
+IP_WEST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}}}' "${control_plane_node_name}" 2>/dev/null)
+
+echo "$EAST_WEST_GATEWAY_IP_EAST | $EAST_WEST_GATEWAY_IP_WEST | $IP_EAST | $IP_WEST"
 
 echo "--- Primary-Primary Service Secrets ---"
 istioctl create-remote-secret \
     --context="kind-${KIND_CLUSTER_EAST}" \
-    --name="${KIND_CLUSTER_EAST}" | \
-    kubectl apply -f - --context="kind-${KIND_CLUSTER_WEST}"
+    --name="${KIND_CLUSTER_EAST}" --server "${IP_EAST}:6443" | \
+    kubectl apply -f - --context="kind-${KIND_CLUSTER_WEST}" 
 
 istioctl create-remote-secret \
     --context="kind-${KIND_CLUSTER_WEST}" \
-    --name="${KIND_CLUSTER_WEST}" | \
-    kubectl apply -f - --context="kind-${KIND_CLUSTER_EAST}"
+    --name="${KIND_CLUSTER_WEST}" --server "${IP_WEST}:7443" | \
+    kubectl apply -f - --context="kind-${KIND_CLUSTER_EAST}" 
 
 echo "--- Primary-Primary Service Mesh Konfiguration ---"
 istioctl remote-clusters --context="kind-${KIND_CLUSTER_EAST}"
@@ -61,4 +43,6 @@ istioctl remote-clusters --context="kind-${KIND_CLUSTER_WEST}"
 
 echo "--- Primary-Primary Service Mesh Konfiguration Abgeschlossen ---"
 echo "Ihre Kind Cluster '$KIND_CLUSTER_EAST' und '$KIND_CLUSTER_WEST' sollten jetzt als Primary-Primary Service Mesh konfiguriert sein."
-echo "Verifizieren Sie die Konfiguration mit 'istioctl proxy-config listeners', 'kubectl get svc -n istio-system', etc."
+echo "Verifizieren Sie die Konfiguration mit:"
+echo "istioctl remote-clusters --context=kind-'$KIND_CLUSTER_EAST'"
+echo "istioctl remote-clusters --context=kind-'$KIND_CLUSTER_EAST'"
