990e7140c0e22f3f4f89b30ca8962a138960c4fb
wlanboy <wlanboy@github.com>
2025-06-16 05:43:34 +0000

    updated list of important ips

--- kind/ips.sh
+++ kind/ips.sh
@@ -10,19 +10,27 @@ KIND_CLUSTER_WEST="west"
 MESH_ID=servicemesh
 MESH_NETWORK=servicenetwork
 
-echo "--- Starte Primary-Primary Service Mesh Konfiguration ---"
+API_SERVER_EXTERNAL_HOSTNAME_EAST="east.gmk.local"
+API_SERVER_EXTERNAL_HOSTNAME_WEST="west.gmk.local"
+API_SERVER_SERVICE_NAMESPACE="default"
 
-# External IPs der East-West Gateways ermitteln
+# External IPs der East-West Gateways und controle-plane ermitteln
 echo "Ermittle External IPs der East-West Gateways..."
 EAST_WEST_GATEWAY_IP_EAST=$(kubectl --context "kind-$KIND_CLUSTER_EAST" get svc "eastwestgateway" -n "$ISTIO_SYSTEM_NAMESPACE" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 EAST_WEST_GATEWAY_IP_WEST=$(kubectl --context "kind-$KIND_CLUSTER_WEST" get svc "eastwestgateway" -n "$ISTIO_SYSTEM_NAMESPACE" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-control_plane_node_name=$(kubectl get nodes --context "kind-$KIND_CLUSTER_WEST" -l node-role.kubernetes.io/control-plane -o custom-columns=NAME:.metadata.name --no-headers)
-echo "$control_plane_node_name"
-IP_EAST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${control_plane_node_name}" 2>/dev/null)
+echo "Ermittle External IPs der East-West Gateways..."
+INGRESS_GATEWAY_IP_EAST=$(kubectl --context "kind-$KIND_CLUSTER_EAST" get svc "istio-ingressgateway" -n "$ISTIO_SYSTEM_NAMESPACE" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+INGRESS_GATEWAY_IP_WEST=$(kubectl --context "kind-$KIND_CLUSTER_WEST" get svc "istio-ingressgateway" -n "$ISTIO_SYSTEM_NAMESPACE" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-control_plane_node_name=$(kubectl get nodes --context "kind-$KIND_CLUSTER_WEST" -l node-role.kubernetes.io/control-plane -o custom-columns=NAME:.metadata.name --no-headers)
-echo "$control_plane_node_name"
-IP_WEST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}}}' "${control_plane_node_name}" 2>/dev/null)
+control_plane_node_name_east=$(kubectl get nodes --context "kind-$KIND_CLUSTER_EAST" -l node-role.kubernetes.io/control-plane -o custom-columns=NAME:.metadata.name --no-headers)
+echo "$control_plane_node_name_east"
+IP_EAST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${control_plane_node_name_east}" 2>/dev/null)
 
-echo "$EAST_WEST_GATEWAY_IP_EAST | $EAST_WEST_GATEWAY_IP_WEST | $IP_EAST | $IP_WEST"
+control_plane_node_name_west=$(kubectl get nodes --context "kind-$KIND_CLUSTER_WEST" -l node-role.kubernetes.io/control-plane -o custom-columns=NAME:.metadata.name --no-headers)
+echo "$control_plane_node_name_west"
+IP_WEST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${control_plane_node_name_west}" 2>/dev/null)
+
+echo "EASTWEST GATEWAYS: $EAST_WEST_GATEWAY_IP_EAST | $EAST_WEST_GATEWAY_IP_WEST"
+echo "INGRESS GATEWAYS: $INGRESS_GATEWAY_IP_EAST | $INGRESS_GATEWAY_IP_WEST"
+echo "NODES: $IP_EAST | $IP_WEST"
