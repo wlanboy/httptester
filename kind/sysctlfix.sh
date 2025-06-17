4d00caa8b9f27e39e2e6f3d5ff26c3095130aa47
wlanboy <wlanboy@github.com>
2025-06-17 05:37:55 +0000

    added check for net.ipv4.ip_forward

--- kind/sysctlfix.sh
+++ kind/sysctlfix.sh
@@ -6,3 +6,7 @@ echo "see: https://kind.sigs.k8s.io/docs/user/known-issues/#pod-errors-due-to-to
 
 sudo sysctl fs.inotify.max_user_watches=524288
 sudo sysctl fs.inotify.max_user_instances=512
+
+# and check ipv4 forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+
